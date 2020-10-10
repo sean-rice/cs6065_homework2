138379967ebf8d5d72819eac9a6ab7c8056497b3
sean rice <contact@sean.ee>
2020-10-10 01:43:26 -0400

    scaffold identity login and registration pages

---
+++ Cs6065_Homework2/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+using System;
+using Cs6065_Homework2.Data;
+using Cs6065_Homework2.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Cs6065_Homework2.Areas.Identity.IdentityHostingStartup))]
+namespace Cs6065_Homework2.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
