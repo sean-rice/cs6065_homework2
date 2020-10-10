2b3205a5d16cc05bdd40f8820e1d5dbea7a007eb
sean rice <contact@sean.ee>
2020-10-10 01:29:26 -0400

    add startup initialization skeleton (future)

---
+++ Cs6065_Homework2/Initialize.cs
@@ -0,0 +1,10 @@
+namespace FitnessWeb
+{
+    public static class Initialize
+    {
+        public static void Initialize()
+        {
+            // TODO: initialization
+        }
+    }
+}
--- Cs6065_Homework2/Program.cs
+++ Cs6065_Homework2/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,7 +14,9 @@ namespace Cs6065_Homework2
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            InitializeDatabase(host);
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,5 +25,22 @@ namespace Cs6065_Homework2
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+        
+        private static void InitializeDatabase(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    // TODO: initialization code goes here (db, admin account, etc.)
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+        }
     }
 }
